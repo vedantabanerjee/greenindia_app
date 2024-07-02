4f3ef3a9aabed5b2c25739af0cd8965be60c8c79
vedantabanerjee <vedantabanerjee01@gmail.com>
2024-07-02 13:47:44 +0530

    initial commit/2.7.24

---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+// added a link and some section and styled the footer section
+
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-green-800 text-white py-2">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+        <p className="text-center mb-2 md:mb-0">
+          &copy; {new Date().getFullYear()} vedantabanerjee
+        </p>
+        <div className='flex'>
+        <p className='text-gray-300 pr-10'>version 0.0(developmental build)</p>
+          <a
+            href="https://github.com/adarsh-jha-dev/h4b"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center text-white hover:text-gray-400 transition duration-300 pr-5"
+          >
+            <FaGithub className="mr-2" />
+            GitHub Repository
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+const Navbar = () => {
+  const navLinks = [
+    { title: "Home", url: "/" },
+    { title: "Dashboard", url: "/dashboard" },
+    { title: "Chat", url: "/chat" },
+    { title: "Info", url: "/info" },
+  ];
+
+  return (
+    <nav className="sticky z-[100] bg-white h-14 inset-x-0 border-b border-gray-200 top-0 w-full backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link className="flex z-40 font-semibold" href={"/"}>
+            <span className="text-green-500">Renewable </span>xchange
+          </Link>
+          <div className="h-full flex items-center space-x-4">
+            {navLinks.map((link) => (
+              <Link
+                className={buttonVariants({ size: "sm", variant: "ghost" })}
+                key={link.url}
+                href={link.url}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
